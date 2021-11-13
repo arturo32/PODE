5cbb60b9331732e80f9b7691b2a33ef6928c9e81
johnvithor <jv.venceslau.c@gmail.com>
2021-11-13 14:02:43 -0300

    Criada classe de adm e atualizada classe de estudante

---
+++ PODE-backend/src/main/java/br/ufrn/imd/pode/model/Administrador.java
@@ -0,0 +1,9 @@
+package br.ufrn.imd.pode.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "administrador")
+public class Administrador extends Usuario {
+}
--- PODE-backend/src/main/java/br/ufrn/imd/pode/model/Estudante.java
+++ PODE-backend/src/main/java/br/ufrn/imd/pode/model/Estudante.java
@@ -1,10 +1,16 @@
 package br.ufrn.imd.pode.model;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
 
 @Entity
 @Table(name = "estudante")
 public class Estudante extends Usuario {
 
+    @NotNull
+    @OneToMany
+    private Set<Vinculo> vinculos;
 }
