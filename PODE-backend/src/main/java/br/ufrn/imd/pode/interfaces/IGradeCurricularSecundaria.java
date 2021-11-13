c0ec1e9dca00047919da62af98c0322b0a757f9c
johnvithor <jv.venceslau.c@gmail.com>
2021-11-13 10:42:25 -0300

    Criadas a interface de grade curricular secundaria e a classe Pes

---
+++ PODE-backend/src/main/java/br/ufrn/imd/pode/interfaces/IGradeCurricularSecundaria.java
@@ -0,0 +1,17 @@
+package br.ufrn.imd.pode.interfaces;
+
+import br.ufrn.imd.pode.model.Disciplina;
+
+import java.util.Set;
+
+public interface IGradeCurricularSecundaria {
+    Integer getChm();
+
+    Integer getCho();
+
+    Set<Disciplina> getDisciplinasObrigatorias();
+
+    Set<Disciplina> getDisciplinasOptativas();
+
+    Boolean concluida(Set<Disciplina> disciplinas);
+}
