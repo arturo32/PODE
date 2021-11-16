e9fbfcfbf3788a26cfa33228d3f20a742ba97152
johnvithor <jv.venceslau.c@gmail.com>
2021-11-16 10:42:49 -0300

    Adiciona o dto abstrato

---
+++ PODE-backend/src/main/java/br/ufrn/imd/pode/model/dto/AbstractDTO.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.pode.model.dto;
+
+public class AbstractDTO {
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
