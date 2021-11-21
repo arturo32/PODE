434bd21717a4118b106cc80fa990d1dbfc2a0a50
johnvithor <jv.venceslau.c@gmail.com>
2021-11-21 11:05:24 -0300

    Move localização de interface

---
+++ PODE-backend/src/main/java/br/ufrn/imd/pode/model/interfaces/IGradeCurricularPrimaria.java
@@ -0,0 +1,34 @@
+package br.ufrn.imd.pode.model.interfaces;
+
+import br.ufrn.imd.pode.model.Disciplina;
+import br.ufrn.imd.pode.model.DisciplinaPeriodo;
+
+import java.util.Set;
+
+public interface IGradeCurricularPrimaria {
+	Integer getChm();
+
+	Integer getCho();
+
+	Integer getChom();
+
+	Integer getChcm();
+
+	Integer getChem();
+
+	Integer getChmaxp();
+
+	Integer getChminp();
+
+	Integer getPrazoMinimo();
+
+	Integer getPrazoMaximo();
+
+	Integer getPrazoEsperado();
+
+	Set<DisciplinaPeriodo> getDisciplinasObrigatorias();
+
+	Set<Disciplina> getDisciplinasOptativas();
+
+	Boolean concluida(Set<Disciplina> disciplinas);
+}
