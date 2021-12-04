85b26c4f6a55af239fbe21021a659dc61414120d
arturo32 <arturofonsecas32@gmail.com>
2021-12-04 09:02:09 -0300

    Faz pequenos ajustes no comentários e importações

--- PODE-backend/src/main/java/br/ufrn/imd/pode/controller/CursoController.java
+++ PODE-backend/src/main/java/br/ufrn/imd/pode/controller/CursoController.java
@@ -5,11 +5,8 @@ import br.ufrn.imd.pode.model.dto.CursoDTO;
 import br.ufrn.imd.pode.service.CursoService;
 import br.ufrn.imd.pode.service.GenericService;
 
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
