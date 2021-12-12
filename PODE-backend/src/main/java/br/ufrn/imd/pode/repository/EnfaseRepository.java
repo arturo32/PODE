a9dc90f90b84989f8bb97563ee224f32b87b19cb
João Vítor <jv.venceslau.c@gmail.com>
2021-12-12 16:17:02 -0300

    Adiciona rota para busca de enfase por curso

--- PODE-backend/src/main/java/br/ufrn/imd/pode/repository/EnfaseRepository.java
+++ PODE-backend/src/main/java/br/ufrn/imd/pode/repository/EnfaseRepository.java
@@ -3,6 +3,11 @@ package br.ufrn.imd.pode.repository;
 import br.ufrn.imd.pode.model.Enfase;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EnfaseRepository extends GenericRepository<Enfase, Long> {
+
+
+	List<Enfase> findByAtivoIsTrueAndCurso_Id(Long curso_id);
 }
