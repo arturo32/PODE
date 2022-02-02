2732627830965ed5a02c36322e9ab65661db8b22
João Vítor <jv.venceslau.c@gmail.com>
2022-02-02 10:13:27 -0300

    adiciona o filtro de disciplinas por pes

--- PODE-app1/src/main/java/br/ufrn/imd/app1/controle/PlanoCursoPesControlador.java
+++ PODE-app1/src/main/java/br/ufrn/imd/app1/controle/PlanoCursoPesControlador.java
@@ -1,8 +1,12 @@
 package br.ufrn.imd.app1.controle;
 
+import br.ufrn.imd.pode.controle.GenericoControlador;
+import br.ufrn.imd.pode.modelo.dto.DisciplinaCursadaDTO;
+import br.ufrn.imd.pode.modelo.dto.DisciplinaDTO;
+import br.ufrn.imd.pode.modelo.dto.PlanoCursoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import br.ufrn.imd.pode.controle.PlanoCursoControlador;
 import br.ufrn.imd.pode.servico.GenericoServico;
@@ -11,9 +15,11 @@ import br.ufrn.imd.app1.modelo.PlanoCursoPes;
 import br.ufrn.imd.app1.modelo.dto.PlanoCursoPesDTO;
 import br.ufrn.imd.app1.servico.PlanoCursoPesServico;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/planos-de-curso")
-public class PlanoCursoPesControlador extends PlanoCursoControlador<PlanoCursoPes, PlanoCursoPesDTO> {
+public class PlanoCursoPesControlador extends GenericoControlador<PlanoCursoPes, PlanoCursoPesDTO, Long> {
 
 	private PlanoCursoPesServico planoCursoPesServico;
 
@@ -25,6 +31,30 @@ public class PlanoCursoPesControlador extends PlanoCursoControlador<PlanoCursoPe
 	@Override
 	protected GenericoServico<PlanoCursoPes, PlanoCursoPesDTO, Long> servico() {
 		return this.planoCursoPesServico;
+	}
+
+	@PostMapping({"/{planoCursoId}/disciplinas-cursadas"})
+	public ResponseEntity<PlanoCursoPesDTO> adicionarDisciplinaCursada(@PathVariable Long planoCursoId, @RequestBody List<DisciplinaCursadaDTO> disciplinas) {
+		return ResponseEntity.ok(this.planoCursoPesServico.converterParaDTO(this.planoCursoPesServico.
+				adicionarDisciplinaCursada(planoCursoId, disciplinas)));
+	}
+
+	@PostMapping({"/{id}/remove-disciplinas-cursadas"})
+	public ResponseEntity<PlanoCursoPesDTO> removerDisciplinaCursada(@PathVariable Long id, @RequestBody List<DisciplinaCursadaDTO> disciplinasDTOS) {
+		return ResponseEntity.ok(this.planoCursoPesServico.converterParaDTO(this.planoCursoPesServico.
+				removerDisciplinaCursada(id, disciplinasDTOS)));
+	}
+
+	@PostMapping({"/{id}/disciplinas-pendentes"})
+	public ResponseEntity<PlanoCursoPesDTO> adicionarDisciplinaPendente(@PathVariable Long id, @RequestBody List<DisciplinaDTO> disciplinasDTOS) {
+		return ResponseEntity.ok(this.planoCursoPesServico.converterParaDTO(this.planoCursoPesServico.
+				adicionarDisciplinaPendente(id, disciplinasDTOS)));
+	}
+
+	@PostMapping({"/{id}/remove-disciplinas-pendentes"})
+	public ResponseEntity<PlanoCursoPesDTO> removerDisciplinaPendente(@PathVariable Long id, @RequestBody List<DisciplinaDTO> disciplinasDTOS) {
+		return ResponseEntity.ok(this.planoCursoPesServico.converterParaDTO(this.planoCursoPesServico.
+				removerDisciplinaPendente(id, disciplinasDTOS)));
 	}
 
 }
--- PODE-app1/src/main/java/br/ufrn/imd/app1/controle/VinculoBTIControlador.java
+++ PODE-app1/src/main/java/br/ufrn/imd/app1/controle/VinculoBTIControlador.java
@@ -1,6 +1,9 @@
 package br.ufrn.imd.app1.controle;
 
+import br.ufrn.imd.pode.controle.GenericoControlador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +16,7 @@ import br.ufrn.imd.app1.servico.VinculoBTIServico;
 
 @RestController
 @RequestMapping("/vinculos")
-public class VinculoBTIControlador extends VinculoControlador<VinculoBTI, VinculoBTIDTO> {
+public class VinculoBTIControlador extends GenericoControlador<VinculoBTI, VinculoBTIDTO, Long> {
 
 	private VinculoBTIServico vinculoBTIServico;
 
@@ -25,6 +28,11 @@ public class VinculoBTIControlador extends VinculoControlador<VinculoBTI, Vincul
 	@Override
 	protected GenericoServico<VinculoBTI, VinculoBTIDTO, Long> servico() {
 		return this.vinculoBTIServico;
+	}
+
+	@GetMapping({"/{idVinculo}/percentual-conclusao"})
+	public Double obterPercentualConclusao(@PathVariable Long idVinculo) {
+		return this.vinculoBTIServico.obterPercentualConclusao(idVinculo);
 	}
 
 }
